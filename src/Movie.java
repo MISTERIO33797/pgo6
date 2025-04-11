1d6a87483c5d2f85c82edfd83417dc47582ce9f8
s33797 <s33797@pjwstl.edu.pl>
2025-04-11 15:03:21 +0200

    commit 2

---
+++ src/Movie.java
@@ -0,0 +1,25 @@
+public class Movie extends LibraryMedia {
+    private String director;
+    private int duration;
+
+    public Movie(String title, int publicationYear, String director, int duration) {
+        super(title, publicationYear);
+        this.director = director;
+        this.duration = duration;
+    }
+
+    @Override
+    public void displayInformation() {
+        super.displayInformation();
+        System.out.println("Director: " + director);
+        System.out.println("Duration: " + duration + " minutes");
+    }
+
+    public void checkDuration() {
+        if (duration > 120) {
+            System.out.println("This is a long movie with " + duration + " minutes.");
+        } else {
+            System.out.println("This is a standard movie with " + duration + " minutes.");
+        }
+    }
+}
